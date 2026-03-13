fc0935636356fea33c1ecab740ab27a229c5ab4b
Pawel Ogrodnik <mkpawell@gmail.com>
2026-03-13 10:46:04 +0100

    fixtures update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 export default defineConfig(
   tseslint.configs.recommended,
   ...mantine,
-  { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', '.next'] },
+  { ignores: ['**/*.{cjs,d.ts,d.mts}', '.next'] },
   {
     files: ['**/*.story.tsx'],
     rules: { 'no-console': 'off' },
